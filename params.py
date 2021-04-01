a1fc7aa13f19cbe4bee33ad52a733d9ecd46272b
greaseddog <greaseddog@hotmail.com>
2021-04-01 22:32:58 +0200

    Changes to parametrization and figures in notebook

--- params.py
+++ params.py
@@ -16,26 +16,26 @@ spec = [('alpha', double), ('T', int32), ('TR', int32), ('TH', int32), ('rho', d
 class par_class:
     def __init__(self):    
         # Scalar parameters
-        self.alpha = 0.1 # Fraction of income earned as holiday pay
-        self.T = 55 # Number of periods
-        self.TR = 40 # Retirement age
-        self.TH = 15 # Holiday payout age
-        self.rho  = 2.0 # CRRA coeficient
-        self.beta = 0.96 # Discount factor
+        self.alpha = 0.125 # Fraction of income earned as holiday pay
+        self.T = 50 # Number of periods
+        self.TR = 35 # Retirement age
+        self.TH = 19 # Holiday payout age
+        self.rho  = 3.0 # CRRA coeficient
+        self.beta = 0.97 # Discount factor
         self.sigma_xi = 0.1 # Standard deviaton of transitory shock
         self.sigma_psi = 0.1 # Standard deviaton of permanent shock
         self.sigma_d = 0.1 # Standard deviation on holiday pay growth
-        self.pi = 0.005 # Probability of low income shock
-        self.mu = 0.0 # Value of low income shock
-        self.R = 1.04 # Asset return factor
-        self.delta = 1.06 # Holiday pay return factor
+        self.pi = 0.075 # Probability of low income shock
+        self.mu = 0.2 # Value of low income shock
+        self.R = 1.03 # Asset return factor
+        self.delta = 1.05 # Holiday pay return factor
         self.m_max = 10.0 # Maximum point in grid for m
         self.m_phi = 1.1 # Curvature of grid for m
         self.h_max = 1.0 # Maximum point in grid for h
         self.h_phi = 1.1 # Curvature of grid for h
-        self.Nxi = 8 # Number of quadrature points for xi
-        self.Npsi = 8 # Number of quadrature points for psi
-        self.Nd = 8 # Number of quadrature points for d
+        self.Nxi = 6 # Number of quadrature points for xi
+        self.Npsi = 6 # Number of quadrature points for psi
+        self.Nd = 6 # Number of quadrature points for d
         self.Nm = 100 # Number of points in grid for m
         self.Nh = 20 # Number of points in grid for h
         self.z_mode = 2 # Mode for z
@@ -45,5 +45,5 @@ class par_class:
         
         # Life cycle
         self.G = np.ones(self.T)
-        self.G[:self.TR-1] = np.linspace(1.05, 0.99, 39)
-        self.G[self.TR-1] = 0.85
+        self.G[:self.TR-1] = np.linspace(1.1, 0.99, self.TR-1)
+        self.G[self.TR-1] = 0.9
